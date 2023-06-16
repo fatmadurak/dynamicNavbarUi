b2906f6f056408a12eb57be4f6ebd0207a3d4b7b
fatmadurak <fatmabadillidurak@gmail.com>
2023-06-16 17:53:22 +0300

    data revized

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import {fetchNavbarData} from "../Api"
-import { useQuery } from 'react-query'
-import "./navbar.css"
+import React from 'react';
+import {fetchNavbarData} from '../Api';
+import { useQuery } from 'react-query';
+import './navbar.css';
 
 function Navbar() {
+  const { isLoading, error, data } = useQuery('navbarData', fetchNavbarData);
 
 
- const { isLoading, error, data } = useQuery('navbarData', fetchNavbarData)
-    
-  
-  if (isLoading) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.message
 
 
   const navItem = (item) => {
     if (item.children && item.children.length > 0) {
       return (
         <li key={item.order} className={item.cssClass}>
-          <a href={item.link} className="nav-link"  style={{ color: item.color }}>
+          <a href={item.link} className="nav-link" style={{ color: item.color }}>
             {item.label}
           </a>
           <ul className="sub-nav">
@@ -39,7 +34,7 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">{data.map(navItem)}</ul>
+      <ul className="navbar-nav">{data && data.map(navItem)}</ul>
     </nav>
   );
 }
