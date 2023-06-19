efc81c88cf9302f2847702897274e2db43943527
fatmadurak <fatmabadillidurak@gmail.com>
2023-06-19 16:42:26 +0300

    navbar codes updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,30 +6,55 @@ import './navbar.css';
 function Navbar() {
   const { isLoading, error, data } = useQuery('navbarData', fetchNavbarData);
   const [openMenu, setOpenMenu] = useState(null);
+  const [isSubMenuVisible, setSubMenuVisible] = useState(false);
 
   const toggleMenu = (index) => {
     if (openMenu === index) {
       setOpenMenu(null);
+      setSubMenuVisible(false);
     } else {
       setOpenMenu(index);
+      setSubMenuVisible(true);
+    }
+  };
+
+  const handleMenuClick = (index) => {
+    if (openMenu === index) {
+      setOpenMenu(null);
+      setTimeout(() => {
+        setSubMenuVisible(false);
+      }, 300); // Animasyon süresi
+    } else {
+      setOpenMenu(index);
+      setSubMenuVisible(true);
     }
   };
 
   const navItem = (item, index) => {
     if (item.children && item.children.length > 0) {
+      const isMenuOpen = openMenu === index;
+
       return (
         <li key={item.order} className={item.cssClass}>
           <a
             href={item.link}
-            className={`nav-link ${openMenu === index ? 'active' : ''}`}
+            className={`nav-link ${isMenuOpen ? 'active' : ''}`}
             style={{ color: item.color }}
-            onClick={() => toggleMenu(index)}
+            onClick={() => handleMenuClick(index)}
           >
             {item.label}
           </a>
-          <ul className={`sub-nav ${openMenu === index ? 'open' : ''}`}>
-            {item.children.map((child) => navItem(child))}
-          </ul>
+          {isMenuOpen && (
+            <ul className={`sub-nav ${isSubMenuVisible ? 'open' : ''}`}>
+              {item.children.map((child) => (
+                <li key={child.order}>
+                  <a href={child.link} className="sub-menu-item">
+                    {child.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
       );
     } else {
