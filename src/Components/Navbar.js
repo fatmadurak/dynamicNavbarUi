b4c3eedd500c8abdb0a11a96df0f41f3e872df47
fatmadurak <fatmabadillidurak@gmail.com>
2023-06-19 16:04:43 +0300

    navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import {fetchNavbarData} from '../Api';
+import React, { useState } from 'react';
+import { fetchNavbarData } from '../Api';
 import { useQuery } from 'react-query';
 import './navbar.css';
 
 function Navbar() {
   const { isLoading, error, data } = useQuery('navbarData', fetchNavbarData);
+  const [openMenu, setOpenMenu] = useState(null);
 
+  const toggleMenu = (index) => {
+    if (openMenu === index) {
+      setOpenMenu(null);
+    } else {
+      setOpenMenu(index);
+    }
+  };
 
-
-
-  const navItem = (item) => {
+  const navItem = (item, index) => {
     if (item.children && item.children.length > 0) {
       return (
         <li key={item.order} className={item.cssClass}>
-          <a href={item.link} className="nav-link" style={{ color: item.color }}>
+          <a
+            href={item.link}
+            className={`nav-link ${openMenu === index ? 'active' : ''}`}
+            style={{ color: item.color }}
+            onClick={() => toggleMenu(index)}
+          >
             {item.label}
           </a>
-          <ul className="sub-nav">
+          <ul className={`sub-nav ${openMenu === index ? 'open' : ''}`}>
             {item.children.map((child) => navItem(child))}
           </ul>
         </li>
@@ -34,7 +45,7 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <ul className="navbar-nav">{data && data.map(navItem)}</ul>
+      <ul className="navbar-nav">{data && data.map((item, index) => navItem(item, index))}</ul>
     </nav>
   );
 }
